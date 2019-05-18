14579e013b00067d0fdd63253a5ec1548b176303
Magiduck <tijsvanlieshout@gmail.com>
2019-05-18 15:24:19 +0200

    UI

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 import kivy
+
 kivy.require('1.10.1')
 
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
 
 
-class MyApp(App):
+class TimerScreen(BoxLayout):
+
+    def __init__(self, **kwargs):
+        self.orientation = 'vertical'
+        super(TimerScreen, self).__init__(**kwargs)
+        countdown = Label(text='30.0 seconds', font_size=72)
+        ok_button = Button(text='I am okay', font_size=32)
+        self.add_widget(countdown)
+        self.add_widget(ok_button)
+
+
+class AFDSApp(App):
 
     def build(self):
-        return Label(text='Hello world')
+        return TimerScreen()
 
 
 if __name__ == '__main__':
-    MyApp().run()
+    AFDSApp().run()
