844ec6454eab8250ecb9f4158952cdb66bd5f7a3
Magiduck <tijsvanlieshout@gmail.com>
2019-05-19 13:32:14 +0200

    alarm/countdown to check if user is okay within 30 seconds

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
+from kivy.clock import Clock
+from kivy.core.audio import SoundLoader
+
+count = 31
 
 
 class TimerScreen(BoxLayout):
@@ -13,10 +17,36 @@ class TimerScreen(BoxLayout):
     def __init__(self, **kwargs):
         self.orientation = 'vertical'
         super(TimerScreen, self).__init__(**kwargs)
-        countdown = Label(text='30.0 seconds', font_size=72)
-        ok_button = Button(text='I am okay', font_size=32)
-        self.add_widget(countdown)
-        self.add_widget(ok_button)
+        self.countdown = Label(text='0', font_size=72, size_hint=(1, .75))
+        self.ok_button = Button(text='I am okay', font_size=16, size_hint=(1, .25))
+        self.ok_button.bind(on_press=self.cancelTimer)
+        self.add_widget(self.countdown)
+        self.add_widget(self.ok_button)
+        Clock.schedule_interval(self.timer, 1)
+
+    def timer(self, dt):
+        global count
+        if count == 0:
+            Clock.unschedule(self.timer)
+        else:
+            self.alarm(self)
+            count -= 1
+            self.countdown.text = str(count)
+
+    def cancelTimer(self, instance):
+        Clock.unschedule(self.timer)
+        self.countdown.text = "Cancelled the countdown!"
+
+    def alarm(self, instance):
+        global count
+        if count == 5:
+            final_sound = SoundLoader.load("Audio/contacting_help.mp3")
+            if final_sound:
+                final_sound.play()
+        elif count % 5 == 0:
+            alarm_sound = SoundLoader.load("Audio/alarm.mp3")
+            if alarm_sound:
+                alarm_sound.play()
 
 
 class AFDSApp(App):
